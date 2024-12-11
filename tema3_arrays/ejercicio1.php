a84df5655dc9862734e2570d4ea513f179dc19ed
jenifera5 <sonnia-55@hotmail.com>
2024-12-11 18:52:51 +0100

    correcion errores

--- tema3_arrays/ejercicio1.php
+++ tema3_arrays/ejercicio1.php
@@ -1,10 +1,11 @@
 <?php
 $num = array(1, 4, 6, 11, 20);
 echo "Los 5 números enteros son: ";
-for ($i = 0; $i < count($num); $i++) {
-    echo $num[$i];  
-    if ($i < count($num) - 1) {
-        echo ", ";  
+
+foreach ($num as $index => $value) {
+    echo $value;
+    if ($index < count($num) - 1) {
+        echo ", ";
     }
 }
 ?>
