bedb25a3e53b2ff0f7221d2e4476c68dec71bd96
jenifera5 <sonnia-55@hotmail.com>
2025-01-09 22:16:18 +0100

    correccion poo1

--- tema4_php_poo1/rectangulo.php
+++ tema4_php_poo1/rectangulo.php
@@ -1,17 +1,12 @@
 <?php
 require_once 'shape.php';
 
-
 class Rectangulo extends Shape {
-    public function __construct($ancho, $alto) {
-        parent::__construct($ancho, $alto); 
-    }
-
     public function area() {
         return $this->ancho * $this->alto; 
     }
 }
- 
 ?>
+
 
 
--- tema4_php_poo1/triangulo.php
+++ tema4_php_poo1/triangulo.php
@@ -1,10 +1,7 @@
-<?php
+<?php 
 require_once 'shape.php';
-class Triangulo extends Shape {
-    public function __construct($ancho, $alto) {
-        parent::__construct($ancho, $alto); // Llama al constructor de Shape
-    }
 
+class Triangulo extends Shape {
     public function area() {
         return ($this->ancho * $this->alto) / 2;
     }
