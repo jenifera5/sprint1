11fbe255804bc2b3f7df844465254d3f5052be5a
jenifer5 <sonnia-55@hotmail.com>
2025-05-24 08:27:05 +0200

    tema 2 correccion 2da

---
+++ Tema3-Phparrays/Tema2-Phpbasico/ejercicio1.php
@@ -0,0 +1,17 @@
+<?php 
+$integer=3;
+$double=5.5;
+$string="Hola";
+$boolean=True;
+
+
+
+echo "integer:" .$integer ."<br>";
+echo "double:"  .$double ."<br>";
+echo "String"   .$string  ."<br>";
+echo "boolean:" .$boolean  ."<br>";
+
+
+define("NOMBRE","Jenifer");
+echo NOMBRE;
+?>
---
+++ Tema3-Phparrays/Tema2-Phpbasico/ejercicio5.php
@@ -0,0 +1,15 @@
+<?php
+$nota=60;
+
+if($nota>= 60){
+echo"Estas en la primera division";
+}else if($nota >= 45 && $nota<= 59){
+echo"Estas en la segunda division";
+}else if($nota >= 33 && $nota<= 44){
+echo"Estas en la tercera division";
+}else{
+   echo"has suspendido"; 
+}
+
+
+?>
